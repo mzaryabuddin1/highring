042392c3d28ac2a8d249e46d52b927a1235d9982
Zaryab <109843326+mzaryabuddin1@users.noreply.github.com>
2024-04-28 15:20:27 +0500

    login api added

--- contollers/user.controller.js
+++ contollers/user.controller.js
@@ -9,7 +9,7 @@ const {
 const app_users = require('../models/users');
 
 const userCtrl = {
-    add: async (req, res) => {
+    register: async (req, res) => {
         try {
             const { first_name, last_name, phone, email, alternate_email, password, terms_and_conditions, user_type } = req.body
 
@@ -52,6 +52,48 @@ const userCtrl = {
             const data = await app_users.findOne({ where : { id }, attributes : ["first_name", "last_name", "phone", "email", "alternate_email", "profile_picture", "user_type", "id"] })
             
             return res.status(200).json({success: 1, msg: "Fetched", data})
+
+        } catch (err) {
+            if (err instanceof ValidationError) {
+                const errorMessages = err.errors.map(err => ({
+                    message: err.message,
+                }));
+                return res.status(400).json({ error: "Validation error", details: errorMessages });
+            }
+
+            return res.status(500).json({ error: "Something Bad happened", details: [{ message: err.message }] })
+        }
+    },
+    login: async (req, res) => {
+        try {
+            const { phone, password } = req.body
+
+            if (!phone || !password )
+                return res.status(400).json({ error: "Validation error", details: [{ message: "All fields required" }] })
+
+            if (!validatePassword(password))
+                return res.status(400).json({ error: "Validation error!", details: [{ message: "Password must be atleast 6 Characters long, contains captial Letter and special character." }] })
+
+            // Validate phone number as MSISDN
+            const msisdnRegex = /^\+?[1-9]\d{1,14}$/; // Regex for MSISDN format
+            if (!msisdnRegex.test(phone))
+                return res.status(400).json({ error: "Validation error", details: [{ message: "Invalid phone number format" }] });
+
+
+            const passwordhash = md5(password)
+            let data = await app_users.findOne({ where : { phone }, attributes:{ exclude : ["alternate_email", "status", "terms_and_conditions","otp","otp_expired","otp_used","createdAt","updatedAt"]} })
+            data = data.toJSON()
+            if (!data)
+                return res.status(400).json({ error: "Validation error", details: [{ message: "This phone number does not exists, please register first!" }] })
+
+            if (data.password !== passwordhash)
+                return res.status(400).json({ error: "Validation error", details: [{ message: "Incorrect password!" }] })
+
+            const access_token = createAccessToken({ id: data.id })
+            
+            delete data.password
+
+            return res.status(200).json({success: 1, msg: "Fetched", data, access_token })
 
         } catch (err) {
             if (err instanceof ValidationError) {
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,7 +2,8 @@ const router = require('express').Router()
 const userCtrl = require('../contollers/user.controller')
 const appauth = require('../middleware/auth')
 
-router.post('/', userCtrl.add)
+router.post('/register', userCtrl.register)
+router.post('/login', userCtrl.login)
 router.get('/', appauth, userCtrl.get)
 
 module.exports = router
