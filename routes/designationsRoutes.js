a1f70c8c87e04fff8e2e03344993014d4187035e
Safee <safeeullah.hussainy@irisdigi.com>
2024-05-01 14:36:53 +0500

    designation module added

---
+++ routes/designationsRoutes.js
@@ -0,0 +1,12 @@
+const router = require('express').Router()
+const Ctrl = require('../contollers/designations.controller')
+const appauth = require('../middleware/auth')
+
+
+router.get('/', appauth, Ctrl.get)
+router.get('/pending', appauth, Ctrl.pending)
+router.post('/', appauth, Ctrl.add_feild)
+router.put('/', appauth, Ctrl.update_feild)
+
+
+module.exports = router
